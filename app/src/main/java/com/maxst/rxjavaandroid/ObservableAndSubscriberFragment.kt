6cbd5f82ab7c0d713e1346fcef79a80349531f0f
Jack <gidools@gmail.com>
2021-06-22 22:10:03 +0900

    Add basic observable and subscriber sample

--- app/src/main/java/com/maxst/rxjavaandroid/ObservableAndSubscriberFragment.kt
+++ app/src/main/java/com/maxst/rxjavaandroid/ObservableAndSubscriberFragment.kt
@@ -12,6 +12,7 @@ import io.reactivex.ObservableEmitter
 import io.reactivex.ObservableOnSubscribe
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
+import java.lang.RuntimeException
 
 
 private const val TAG = "ObservableAndSubscriber"
@@ -31,15 +32,14 @@ class ObservableAndSubscriberFragment: Fragment() {
     override fun onResume() {
         super.onResume()
 
-        val simpleObservable = Observable.create(object : ObservableOnSubscribe<String> {
-            override fun subscribe(emitter: ObservableEmitter<String>) {
-                emitter.onNext("Hello RxAndroid!")
-                emitter.onComplete()
-            }
+        val simpleObservable = Observable.create(ObservableOnSubscribe<String> { emitter ->
+            // emitter.onError(RuntimeException("Error!!"))
+            emitter.onNext("Hello RxAndroid!")
+            emitter.onComplete()
         })
 
-
         setSubscriber1(simpleObservable)
+        setSubscriber2(simpleObservable)
     }
 
     private fun setSubscriber1(observable: Observable<String>) {
@@ -51,7 +51,7 @@ class ObservableAndSubscriberFragment: Fragment() {
 
                 override fun onNext(text: String) {
                     Log.d(TAG, "onNext")
-                    (view!!.findViewById(R.id.textView1) as TextView).text = text
+                    (requireView().findViewById(R.id.textView1) as TextView).text = text
                 }
 
                 override fun onError(e: Throwable) {
@@ -59,8 +59,19 @@ class ObservableAndSubscriberFragment: Fragment() {
                 }
 
                 override fun onComplete() {
-                    Log.d(TAG, "complete!")
+                    Log.d(TAG, "onComplete!")
                 }
             })
+    }
+
+    private fun setSubscriber2(observable: Observable<String>) {
+        val result = observable.subscribe(
+            { inputText ->
+                Log.d(TAG, "OnNext consumer")
+                (requireView().findViewById(R.id.textView2) as TextView).text = "length: " + inputText.length
+            },
+            { Log.d(TAG, "OnError consumer") },
+            { Log.d(TAG, "OnComplete consume") }
+        )
     }
 }
